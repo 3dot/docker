ee8e8ee624b863c453f861e4eccb5db58749a5f8
Miran Hojnik <miran@3dot.net>
2015-01-05 19:17:55 +0100

    phpmyadmin updates

--- applications/phpmyadmin/data/http/config.inc.php
+++ applications/phpmyadmin/data/http/config.inc.php
@@ -3,4 +3,4 @@ $cfg['blowfish_secret'] = '';
 $cfg['PmaNoRelation_DisableWarning'] = true;
 $i=0;
 $i++;
-$cfg['Servers'][$i]['host'] = $_ENV["MYSQL_HOST"];
+$cfg['Servers'][$i]['host'] = 'mysql_host_name';
---
+++ applications/phpmyadmin/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sed "s/mysql_host_name/${MYSQL_HOST}/" -i /data/http/config.inc.php
